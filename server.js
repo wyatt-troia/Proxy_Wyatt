badcb5823ae9fd58235b57d6fb15fd3bf675507f
Wyatt <mrwnt10@gmail.com>
2018-12-03 12:23:23 -0500

    Add static server-side rendered bookings component

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const axios = require("axios");
 const parser = require("body-parser");
 const app = express();
 const port = 9000;
+const urls = require("./urls");
 
 app.use(parser.json());
 app.use(morgan("dev"));
@@ -15,6 +16,79 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.all("/*", function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   next();
+});
+
+const ssr = async id => {
+  let props = await models.getListing(id);
+  let component = React.createElement(Booking, props);
+  let ssr_html = ReactDOMServer.renderToString(component);
+  return { ssr_html, props };
+};
+
+app.get("/listings", async (req, res) => {
+  let id = req.query.id;
+  console.log(`${urls.bookings.ssr}?id=${id}`);
+  let results = await axios(`${urls.bookings.ssr}?id=${id}`);
+  let { ssr_html: booking_html, props: booking_props } = results.data;
+
+  res.send(`<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <title>Staybnb</title>
+      <link rel="stylesheet" type="text/css" href="/styles.css" />
+  
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="http://ec2co-ecsel-14mqx2j7r6mip-726135605.us-east-2.elb.amazonaws.com:9005/guestBar.css"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="http://ec2co-ecsel-14mqx2j7r6mip-726135605.us-east-2.elb.amazonaws.com:9005/_datepicker.css"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="http://ec2co-ecsel-14mqx2j7r6mip-726135605.us-east-2.elb.amazonaws.com:9005/flexboxgrid2.css"
+      />
+      <link type="text/css" rel="stylesheet" href="styles.css" />
+  
+      <link rel="icon" type="image/png" href="/favicon.png" />
+    </head>
+    <body>
+      <div id="description"></div>
+      <div class="container-left">
+        <div id="reviews"></div>
+        <div id="neighborhood"></div>
+      </div>
+      <div class="container-right">${booking_html}<div id="booking"></div></div>
+      <script
+        crossorigin
+        src="https://unpkg.com/react@16/umd/react.development.js"
+      ></script>
+      <script
+        crossorigin
+        src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"
+      ></script>
+      <!-- <script src="http://3.16.89.66/app.js"></script> -->
+      <!-- Dev's bundle -->
+      <!-- <script src="http://52.14.238.117/bundle.js"></script> -->
+      <!-- Louis's bundle -->
+      <!-- <script src="${urls.bookings.bundle}"></script> -->
+      <!-- Stacy's bundle -->
+      <!--
+        <script src="http://18.218.27.164/bundle.js"></script>
+        <script>
+          ReactDOM.render(
+            React.createElement(Neighborhood),
+            document.getElementById('neighborhood')
+          );
+        </script>
+      -->
+    </body>
+  </html>`);
 });
 
 // Add DAVID's API endpoints
---
+++ urls.js
@@ -0,0 +1,7 @@
+module.exports = {
+  bookings: {
+    ssr:
+      "http://ec2co-ecsel-14mqx2j7r6mip-726135605.us-east-2.elb.amazonaws.com:9005/renderBooking",
+    bundle: "https://s3.amazonaws.com/topbunk/bundle.js"
+  }
+};
